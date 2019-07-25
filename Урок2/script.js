b04af74924558806a88c74477ed068291493f77b
helcher <46494815+helcher@users.noreply.github.com>
2019-07-25 17:59:09 +0400

    Урок2. Добавлена автоподстановка текущей даты в URL

--- Урок2/script.js
+++ Урок2/script.js
@@ -1,36 +1,42 @@
 
 const result = document.querySelector('.block2');
 const button = document.getElementById('btn')
-button.addEventListener('click', ()=> {
-	
-	fetch('https://newsapi.org/v2/everything?q=bitcoin&from=2019-06-24&sortBy=publishedAt&apiKey=018a8d8963b14086b414bea6d274fa0c', {method: 'GET'}).then(response => response.json()).then(data => {
+button.addEventListener('click', () => {
+	const date = new Date();
+	const formattedDate = date.toLocaleDateString('en-GB', {
+		day: 'numeric', month: 'numeric', year: 'numeric'
+	}).split('/').reverse().join('-')
+	console.log(formattedDate);
+	let URL = `https://newsapi.org/v2/everything?q=bitcoin&from=${formattedDate}&sortBy=publishedAt&apiKey=018a8d8963b14086b414bea6d274fa0c`
+
+	fetch(URL, { method: 'GET' }).then(response => response.json()).then(data => {
 		const item = data.articles;
 		console.log(item)
 		if (!item.length) {
-            result.innerHTML = '<p>Nothing</p>';
-          } else {
-            result.innerHTML = '';
-            item.map((item) => {
+			result.innerHTML = '<p>Nothing</p>';
+		} else {
+			result.innerHTML = '';
+			item.map((item) => {
 				const div = document.createElement('div');
 				div.classList.add('article')
 				result.append(div);
-				
-              const p1 = document.createElement('p');
-			  p1.classList.add('author');
-              p1.textContent = item.author;
-			  
-			  const p2 = document.createElement('p');
-			  p2.classList.add('content')
-              p2.textContent = item.content;
-			  
-              const img = document.createElement('img');	
-			  img.src = item.urlToImage;
-              
-			  div.append(img);
-			  div.append(p2);
-			  div.append(p1);
-            });
-          }
-	} ) 
+
+				const p1 = document.createElement('p');
+				p1.classList.add('author');
+				p1.textContent = item.author;
+
+				const p2 = document.createElement('p');
+				p2.classList.add('content')
+				p2.textContent = item.content;
+
+				const img = document.createElement('img');
+				img.src = item.urlToImage;
+
+				div.append(img);
+				div.append(p2);
+				div.append(p1);
+			});
+		}
+	})
 
 })
