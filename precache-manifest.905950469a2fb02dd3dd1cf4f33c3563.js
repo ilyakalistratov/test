30eeefb76bd061ba8886dfadd10b4b084a2cc693
helcher <46494815+helcher@users.noreply.github.com>
2019-08-25 18:14:39 +0400

    Updates

---
+++ precache-manifest.905950469a2fb02dd3dd1cf4f33c3563.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3959c8a2de962599d7968aa44d47e1f",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "f182de13c55f55def9e6",
+    "url": "/test/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "d41007befdeb6ea36252",
+    "url": "/test/static/css/main.9c4d9839.chunk.css"
+  },
+  {
+    "revision": "f182de13c55f55def9e6",
+    "url": "/test/static/js/2.0722994a.chunk.js"
+  },
+  {
+    "revision": "d41007befdeb6ea36252",
+    "url": "/test/static/js/main.bc3de86a.chunk.js"
+  },
+  {
+    "revision": "19fceae57ce5f35a48db",
+    "url": "/test/static/js/runtime~main.352083b1.js"
+  },
+  {
+    "revision": "23ac370310efa1b82a3e0003e6589898",
+    "url": "/test/static/media/bg.23ac3703.jpg"
+  },
+  {
+    "revision": "36df36c3628c9d18ba57e8a9fb1aace9",
+    "url": "/test/static/media/html5.36df36c3.svg"
+  },
+  {
+    "revision": "ce74d4e71162817a55eca2c7fe926ebc",
+    "url": "/test/static/media/js.ce74d4e7.svg"
+  },
+  {
+    "revision": "16a3593b2cb384cd0443e24bda8b0883",
+    "url": "/test/static/media/laptop.16a3593b.svg"
+  },
+  {
+    "revision": "d7f290f3a996de6cc09d58c89598b195",
+    "url": "/test/static/media/preloader.d7f290f3.svg"
+  }
+]);
