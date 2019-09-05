c897ef13d083808ddc01aab51658f09d3180d014
helcher <46494815+helcher@users.noreply.github.com>
2019-09-05 19:16:36 +0400

    [10] Рефакторинг. Преобразование классовых компонент в функциональные

--- Урок6/Portfolio/src/components/PageSections/Examples.jsx
+++ Урок6/Portfolio/src/components/PageSections/Examples.jsx
@@ -7,59 +7,55 @@ import NewsContainer from '../Projects/News/NewsContainer';
 import LoadingWeatherBg from '../Projects/LoadingWeatherBg/LoadingWeatherBg';
 import { observer, inject } from 'mobx-react';
 
-@inject("store")
-@observer
-class Examples extends React.Component {
-  render() {
-    const { theme } = this.props.store;
-    return (
-      <Layout id='Examples' style={{ backgroundColor: `${theme.secondSectionBackground}`, color: `${theme.colorParagraph}` }}>
-        <Row>
-          <Col>
-            <h2 style={{ textAlign: 'center', fontSize: '32px', color: `${theme.colorHeader}`, margin: '20px 0' }}>Примеры работ</h2>
-          </Col>
-        </Row>
-        <Row type="flex" justify="center" gutter={48} style={{ margin: '20px 0 30px 0' }}>
-          <Col xs={{ span: 24 }} lg={{ span: 8 }}>
-            <Card >
-              <TaskContainer />
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} lg={{ span: 8 }}>
-            <Card>
-              <ConvertContainer />
-            </Card>
-          </Col>
-        </Row>
-        <Row type="flex" justify="center">
-          <Col xs={{ span: 16 }}> <hr /></Col>
-        </Row>
-        <Row type="flex" justify="center" gutter={48} style={{ margin: '20px 0 30px 0' }}>
-          <Col xs={{ span: 24 }} lg={{ span: 8 }}>
-            <Card>
-              <Weather />
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} lg={{ span: 8 }}>
-            <Card>
-              <NewsContainer />
-            </Card>
-          </Col>
-        </Row>
-        <Row type="flex" justify="center">
-          <Col xs={{ span: 16 }}> <hr /></Col>
-        </Row>
-        <Row type="flex" justify="center" gutter={48} style={{ margin: '20px 0 30px 0' }}>
-          <Col xs={{ span: 24 }} lg={{ span: 8 }}>
-            <Card>
-              <LoadingWeatherBg />
-            </Card>
-          </Col>
-          <Col xs={{ span: 24 }} lg={{ span: 8 }}>
-          </Col>
-        </Row>
-      </Layout>
-    )
-  }
-}
+const Examples = inject('store')(observer(props => {
+  const { theme } = props.store;
+  return (
+    <Layout id='Examples' style={{ backgroundColor: `${theme.secondSectionBackground}`, color: `${theme.colorParagraph}` }}>
+      <Row>
+        <Col>
+          <h2 style={{ textAlign: 'center', fontSize: '32px', color: `${theme.colorHeader}`, margin: '20px 0' }}>Примеры работ</h2>
+        </Col>
+      </Row>
+      <Row type="flex" justify="center" gutter={48} style={{ margin: '20px 0 30px 0' }}>
+        <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+          <Card >
+            <TaskContainer />
+          </Card>
+        </Col>
+        <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+          <Card>
+            <ConvertContainer />
+          </Card>
+        </Col>
+      </Row>
+      <Row type="flex" justify="center">
+        <Col xs={{ span: 16 }}> <hr /></Col>
+      </Row>
+      <Row type="flex" justify="center" gutter={48} style={{ margin: '20px 0 30px 0' }}>
+        <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+          <Card>
+            <Weather />
+          </Card>
+        </Col>
+        <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+          <Card>
+            <NewsContainer />
+          </Card>
+        </Col>
+      </Row>
+      <Row type="flex" justify="center">
+        <Col xs={{ span: 16 }}> <hr /></Col>
+      </Row>
+      <Row type="flex" justify="center" gutter={48} style={{ margin: '20px 0 30px 0' }}>
+        <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+          <Card>
+            <LoadingWeatherBg />
+          </Card>
+        </Col>
+        <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+        </Col>
+      </Row>
+    </Layout>
+  )
+}))
 export default Examples;
