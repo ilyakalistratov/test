15eb7ebd420a0621a940652a178d6d01f705ae19
helcher <46494815+helcher@users.noreply.github.com>
2019-10-01 21:23:40 +0400

    [7.2] Mobx-store-tree реализован Todo

---
+++ Урок7/social/src/containers/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react'; //These functions make our components observable and be able to use the store
+import TodoList from '../components/TodoList';
+import Form from '../components/Form';
+import logo from '../logo.svg';
+import '../App.css';
+
+class App extends Component {
+  render() {
+    const { store } = this.props;
+
+    return (
+      <div className="App">
+
+        <div>
+          <h3 className="subtitle">Make a new To do</h3>
+          <Form store={store} />
+        </div>
+        <div className="card-container">
+          {store.Todo.map((todo, i) => (
+            <TodoList todo={todo} key={i} />))}
+        </div>
+      </div>
+    );
+  }
+}
+export default inject('store')(observer(App));
